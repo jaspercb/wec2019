4eac08a5e784a24ccd34e8a32a84edd614a04d9c
Sean Purcell <iburinoc@gmail.com>
2019-07-19 19:46:57 -0400

    Update game struct

--- src/game.py
+++ src/game.py
@@ -1,16 +1,26 @@
 class Game(object):
-    def __init__(self, team1, team2):
-        self.team1 = team1
-        self.team2 = team2
-
-        # will be None until winner is determined
-        self.winner = None
+    def __init__(self, team1=None, team2=None):
+        self.teams = (team1, team2)
+        self.score = (0, 0)
 
     def getTeams(self):
-        return (self.team1, self.team2)
+        return self.teams
 
-    def reportResult(self, winner):
-        if winner not in self.getTeams():
-            raise ValueError()
+    def getWinner(self):
+        if self.isBye():
+            return self.teams[0]
+        return max(zip(self.score, self.teams))[1]
 
-        self.winner = winner
+    def getLoser(self):
+        if self.isBye():
+            return None
+        return min(zip(self.score, self.teams))[1]
+
+    def getOpponent(self, team):
+        return self.teams[0] if team != self.teams[0] else self.teams[1]
+
+    def isBye(self):
+        return self.teams[1] == None
+
+    def __repr__(self):
+        return f'teams: {self.teams}, score: {self.score}'
