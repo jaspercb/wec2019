93e80105acf77ec6bd847d095980f5aea6217242
Celine O'Neil <celineloneil@gmail.com>
2019-07-19 19:49:45 -0400

    track unplayed games

--- src/tournament.py
+++ src/tournament.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 class Tournament(object):
     def __init__(self, teams, round_generator):
+        self.round_generator = round_generator
         self.next_game_id = 0
 
         # win/loss record for each team, ordered by original seed
@@ -12,12 +13,21 @@ class Tournament(object):
         # (games in a round may be played out of order)
         self.current_round = OrderedDict()
 
-        for game in round_generator.generateGames(self):
+        # will store games in order they were played
+        self.past_games = OrderedDict()
+
+        # set of gameids representing unplayed games in the current round
+        self.unplayed_games = set()
+
+        self.startNextRound()
+
+    # will not verify that all games in past round were played
+    def startNextRound(self):
+        for game in self.round_generator.generateGames(self):
             self.current_round[self.next_game_id] = game
             self.next_game_id += 1
 
-        # will store games in order they were played
-        self.past_games = OrderedDict()
+        self.unplayed_games = set(self.currentGames())
 
     # games in current round
     def currentGames(self):
@@ -30,8 +40,16 @@ class Tournament(object):
             return self.past_games[gameid]
         raise KeyError()
 
-    def reportResult(self, gameid, winner):
-        self.current_round[gameid].reportResult(winner)
+    # |score| is a tuple (team1_score, team2_score)
+    def setScore(self, gameid, score):
+        self.current_round[gameid].score = score
+
+        try:
+            self.unplayed_games.remove(gameid)
+        except KeyError:
+            pass
+        else:
+            self.past_games.append(gameid)
 
     def getTeamRecord(self, string):
         self.team_records[string]
