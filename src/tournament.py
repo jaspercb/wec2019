896d5d92b4974bf5232232931198e35b1a656d40
Sean Purcell <iburinoc@gmail.com>
2019-07-19 18:13:22 -0400

    Super simple tournament stub

---
+++ src/tournament.py
@@ -0,0 +1,13 @@
+class Tournament:
+
+    def __init__(self, teams):
+        self.teams = teams
+
+    def currentGames(self):
+        return []
+
+    def reportResult(self, gameid, winner):
+        pass
+
+    def playersIn(self, gameid):
+        return []
